6cb20846aa7829e813a9d600ac55f72076a4b751
Seregon <109359355+seregonwar@users.noreply.github.com>
2023-06-04 16:48:06 +0200

    Add files via upload

--- AllCrypter.py
+++ AllCrypter.py
@@ -6,13 +6,12 @@ import sys
 import logging
 from passlib.hash import bcrypt
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.primitives import hashes, hmac 
+from cryptography.hazmat.primitives import hashes, hmac
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
-from randomgen import SecureRandom
 import ssl
-import pip._internal
+import pkg_resources
 # Kivy imports
 from kivy.config import Config
 Config.set('kivy', 'exit_on_escape', '0')
@@ -26,36 +25,31 @@ from kivy.uix.filechooser import FileChooserListView
 
 
 def check_dependencies():
-    try:
-        import pip
-    except ImportError:
-        print("Pip not found. Please install pip.")
-        sys.exit(1)
-
     required_packages = [
         'cryptography',
         'passlib',
         'bcrypt',
-        'randomgen',
         'securefile',
         'kivy'
     ]
 
-    installed_packages = [pkg.key for pkg in pip._internal.get_installed_distributions()]
+    installed_packages = [pkg.key for pkg in pkg_resources.working_set]
 
     for package in required_packages:
         if package not in installed_packages:
-            pip.main(['install', package])
+            os.system(f"pip install {package}")
 
-    installed_packages = [pkg.key for pkg in pip.get_installed_distributions()]
+    installed_packages = [pkg.key for pkg in pkg_resources.working_set]
 
     for package in required_packages:
         if package not in installed_packages:
-            pip.main(['install', package])
+            print(f"Failed to install {package}. Please install it manually.")
+            sys.exit(1)
 
 
 check_dependencies()
 
+# Resto del codice come prima
 
 def generate_random_password(length=12):
     characters = string.ascii_letters + string.digits + string.punctuation
@@ -67,21 +61,8 @@ def generate_random_password(length=12):
 # la limitazione dei tentativi di accesso, la sanitizzazione dei dati in input
 # e le connessioni TLS/SSL.
 
-# Usa TLS 1.2 o versioni successive
-context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-context.minimum_version = ssl.TLSVersion.TLSv1_2
-context.load_verify_locations("ca.pem")
-
 # Imposta un limite di 3 tentativi di accesso 
 login_attempts = 3
-
-# Disabilita la modalità di debug e di audit
-logging.disable(logging.DEBUG)  
-logging.disable(logging.AUDIT)
-
-# Esegui con privilegi minimi 
-os.setgid(1000)  
-os.setuid(1000) 
 
 # Cancella in modo sicuro
 # secure_file_deletion(file_path)  # Commentata poiché 'file_path' non è definita
@@ -122,15 +103,17 @@ def decrypt_file(encrypted_file_path, key, algorithm, nonce_length):
         encrypted_data = encrypted_file.read()
 
     nonce = encrypted_data[:nonce_length]
-    cipher = Cipher(algorithm(key), modes.GCM(nonce), backend=default_backend())
+    tag = encrypted_data[nonce_length:nonce_length+16]  # Il tag di autenticazione ha una lunghezza fissa di 16 byte
+    cipher = Cipher(algorithm(key), modes.GCM(nonce, tag), backend=default_backend())
     decryptor = cipher.decryptor()
-    decrypted_data = decryptor.update(encrypted_data[nonce_length:]) + decryptor.finalize()
+    decrypted_data = decryptor.update(encrypted_data[nonce_length+16:]) + decryptor.finalize()
 
-    decrypted_file_path = encrypted_file_path[:-10]  # Remove the ".encrypted" extension
+    decrypted_file_path = encrypted_file_path[:-10]  # Rimuovi l'estensione ".encrypted"
     with open(decrypted_file_path, "wb") as decrypted_file:
         decrypted_file.write(decrypted_data)
 
     return decrypted_file_path
+
 
 
 class EncryptionApp(App):
